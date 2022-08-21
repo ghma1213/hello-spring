3f0ed51dcc6c7043706e2053a13480e6f587a0bb
kyunghwanma <makh1213@naver.com>
2022-08-21 23:15:25 +0900

    lang 파라미터 설정

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -2,12 +2,23 @@ package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.servlet.http.HttpSession;
+import java.util.Locale;
+import java.util.Objects;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(@RequestParam(value = "lang", required = false) String lang, HttpSession session) {
+        if (Objects.equals(lang, "") ) {
+            session.setAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME, Locale.KOREA);
+        } else {
+            session.setAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME, Locale.ENGLISH);
+        }
         return "home";
     }
 }
