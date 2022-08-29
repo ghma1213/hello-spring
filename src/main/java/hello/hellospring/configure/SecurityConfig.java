ee46a14ceb161ea82f978ab5a2730d42a317a426
Gyeonghwan <ghma1213@gmail.com>
2022-08-29 09:01:49 +0900

    리멤버미 테스트

--- src/main/java/hello/hellospring/configure/SecurityConfig.java
+++ src/main/java/hello/hellospring/configure/SecurityConfig.java
@@ -1,19 +1,33 @@
 package hello.hellospring.configure;
 
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
 
 @Configuration
 @EnableWebSecurity // 웹 보안 활성화를 위한 Annotation
 public class SecurityConfig {
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .rememberMe() // rememberMe 기능 작동
+                .key("rememberMeTest")
+                .rememberMeParameter("remember-me") // 기본 파라미터 명은 remember-me
+                .tokenValiditySeconds(3600) // 기본값은 14일
+                .alwaysRemember(true) // rememberMe 기능이 활성화되지 않아도 항상 실행. 기본값은 false
+                .userDetailsService(userDetailsService) // rememberMe에서 시스템에 있는 사용자 계정을 조회할 때 사용할 클래스
+            .and()
                 .csrf().disable() // 사이트 간 위조 요청 disable
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위한 옵션 disable
             .and()
